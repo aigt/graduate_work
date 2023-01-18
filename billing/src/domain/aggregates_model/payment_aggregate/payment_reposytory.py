fff33f7cd9e20b1885c2aa93ef3c188a95f31689
aigt <ai-kr@yandex.ru>
2023-01-18 22:36:03 +0500

    Добавлен агрегат данных платежа

---
+++ billing/src/domain/aggregates_model/payment_aggregate/payment_reposytory.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+from domain.aggregates_model.payment_aggregate.payment import Payment
+
+
+class PaymentRepository(ABC):
+    """Интерфейс репозиториев платежей."""
+
+    @abstractmethod
+    def create_payment(self) -> Payment:
+        """Создать платёж.
+
+        Returns:
+            Payment: Платёж.
+        """
+
+    @abstractmethod
+    def get_by_external_id(self, external_id: str) -> Payment:
+        """Найти платёж по идентификатору внешнего сервиса.
+
+        Args:
+            external_id (str): Идентификатор платежа в платёжной системе.
+
+        Returns:
+            Payment: Платёж.
+        """
