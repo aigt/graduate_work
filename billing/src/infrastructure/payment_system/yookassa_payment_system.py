05316e7a2a2ce0ceb68ec65964712d7631902bda
aigt <ai-kr@yandex.ru>
2023-01-22 12:24:19 +0500

    Актуализация в соответствии с изменениями интерфейса

--- billing/src/infrastructure/payment_system/yookassa_payment_system.py
+++ billing/src/infrastructure/payment_system/yookassa_payment_system.py
@@ -1,14 +1,23 @@
 from domain.aggregates_model.external_payment_aggregate.external_payment import (
     ExternalPayment,
 )
+from domain.aggregates_model.external_payment_aggregate.external_payment_amount import (
+    ExternalPaymentAmount,
+)
 from domain.aggregates_model.external_payment_aggregate.external_payment_id import (
     ExternalPaymentId,
 )
 from domain.aggregates_model.external_refund_aggregate.external_refund import (
     ExternalRefund,
 )
+from domain.aggregates_model.external_refund_aggregate.external_refund_amount import (
+    ExternalRefundAmount,
+)
 from domain.aggregates_model.external_refund_aggregate.external_refund_id import (
     ExternalRefundId,
+)
+from domain.aggregates_model.external_refund_aggregate.external_refund_payment_id import (
+    ExternalRefundPaymentId,
 )
 from domain.services.payment_system import PaymentSystem
 
@@ -20,8 +29,11 @@ class YookassaPaymentSystem(PaymentSystem):
     https://yookassa.ru/developers/payment-acceptance/integration-scenarios/smart-payment
     """
 
-    async def create_payment(self) -> ExternalPayment:
+    async def create_payment(self, amount: ExternalPaymentAmount) -> ExternalPayment:
         """Создать платёж.
+
+        Args:
+            amount (ExternalPaymentAmount): Сумма платежа.
 
         Returns:
             ExternalPayment: Созданный платёж.
@@ -32,7 +44,7 @@ class YookassaPaymentSystem(PaymentSystem):
         """Получить список платежей зарегестрированных в платёжной системе.
 
         Returns:
-            list[ExternalPayment]: Список платежей.
+            list[ExternalPayment]: Список платежей в актуальном состоянии.
         """
         return NotImplemented
 
@@ -43,7 +55,7 @@ class YookassaPaymentSystem(PaymentSystem):
             payment_id (ExternalPaymentId): Идентификатор платежа.
 
         Returns:
-            ExternalPayment: Платёж в системе.
+            ExternalPayment: Платёж в системе в актуальном состоянии.
         """
         return NotImplemented
 
@@ -54,7 +66,7 @@ class YookassaPaymentSystem(PaymentSystem):
             payment_id (ExternalPaymentId): Идентификатор платежа.
 
         Returns:
-            ExternalPayment: Платёж в системе.
+            ExternalPayment: Платёж в системе в актуальном состоянии.
         """
         return NotImplemented
 
@@ -65,7 +77,7 @@ class YookassaPaymentSystem(PaymentSystem):
             payment_id (ExternalPaymentId): Идентификатор платежа.
 
         Returns:
-            ExternalPayment: Платёж в системе.
+            ExternalPayment: Платёж в системе в актуальном состоянии.
         """
         return NotImplemented
 
@@ -73,12 +85,16 @@ class YookassaPaymentSystem(PaymentSystem):
         """Получить список возвратов зарегестрированных в платёжной системе.
 
         Returns:
-            list[ExternalRefund]: Список возвратов.
+            list[ExternalRefund]: Список возвратов в актуальном состоянии.
         """
         return NotImplemented
 
-    async def create_refund(self) -> ExternalRefund:
+    async def create_refund(self, amount: ExternalRefundAmount, payment_id: ExternalRefundPaymentId) -> ExternalRefund:
         """Создать возврат.
+
+        Args:
+            amount (ExternalRefundAmount): Сумма возврата.
+            payment_id (ExternalRefundPaymentId): Идентификатор платежа, на который осуществляется возврат.
 
         Returns:
             ExternalRefund: Созданный возврат.
@@ -89,9 +105,9 @@ class YookassaPaymentSystem(PaymentSystem):
         """Получить информацию о возврате в платёжной системе.
 
         Args:
-            refund_id (ExternalRefundId): Идентификатор возврате.
+            refund_id (ExternalRefundId): Идентификатор возврата.
 
         Returns:
-            ExternalRefund: Возврат в системе.
+            ExternalRefund: Возврат в системе в актуальном состоянии.
         """
         return NotImplemented
