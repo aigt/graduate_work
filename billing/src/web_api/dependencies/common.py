5a5167aba13416f47910af25752315121da003a1
aigt <ai-kr@yandex.ru>
2023-01-23 21:56:46 +0500

    Добавлен базовый файл зависимостей

---
+++ billing/src/web_api/dependencies/common.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+from web_api.core.config import Settings
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    """Фабрика настроек.
+
+    Returns:
+        Settings: Класс с настройками приложения.
+    """
+    return Settings()
