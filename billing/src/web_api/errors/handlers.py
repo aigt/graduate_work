a5bdd7e174b3e5baa5acdaef881f7a5310842758
aigt <ai-kr@yandex.ru>
2023-01-23 21:52:26 +0500

    Добавлены базовые файлы ошибок

---
+++ billing/src/web_api/errors/handlers.py
@@ -0,0 +1,28 @@
+from errors.exceptions import HTTPException
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
+async def app_http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
+    """Хэндлер для HTTP исключений приложения.
+
+    Args:
+        request (Request): Запрос.
+        exc (HTTPException): Исключение возникшее в ходе обработки запроса.
+
+    Returns:
+        JSONResponse: Детальный ответ.
+    """
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
+
+
+def install_exception_handlers(app: FastAPI) -> None:
+    """Установить хэндлеры исключений в FastAPI приложение.
+
+    Args:
+        app (FastAPI): Приложение.
+    """
+    app.exception_handler(HTTPException)(app_http_exception_handler)
