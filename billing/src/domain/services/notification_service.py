2d76561a72a395f9c9dfa84a13e8682dbfb5737a
aigt <ai-kr@yandex.ru>
2023-01-23 21:10:28 +0500

    Обновлены типы данных сервисов

--- billing/src/domain/services/notification_service.py
+++ billing/src/domain/services/notification_service.py
@@ -1,24 +1,27 @@
 from abc import ABC, abstractmethod
-from decimal import Decimal
-from uuid import UUID
+
+from billing.src.domain.aggregates_model.external_refund_aggregate.external_refund_amount import (
+    ExternalRefundAmount,
+)
+from billing.src.domain.aggregates_model.user_aggregate.user_id import UserId
 
 
 class NotificationService(ABC):
     """Сервис оповещений."""
 
     @abstractmethod
-    async def notify_user_about_payment(self, user_id: UUID) -> None:
+    async def notify_user_about_payment(self, user_id: UserId) -> None:
         """Оповестить пользователя о платеже.
 
         Args:
-            user_id (UUID): Id пользователя.
+            user_id (UserId): Id пользователя.
         """
 
     @abstractmethod
-    async def notify_user_about_refund(self, user_id: UUID, amount: Decimal) -> None:
+    async def notify_user_about_refund(self, user_id: UserId, amount: ExternalRefundAmount) -> None:
         """Оповестить пользователя о возврате.
 
         Args:
-            user_id (UUID): Id пользователя.
-            amount (Decimal): Сумма возврата.
+            user_id (UserId): Id пользователя.
+            amount (ExternalRefundAmount): Сумма возврата.
         """
