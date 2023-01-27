e6ccded21579252a1b8e9c1412e346379a19915a
wood <amper.sil.tok@gmail.com>
2023-01-27 08:12:35 +0300

    Добавлен proto для сервиса авторизации и класс для работы с сервисом авторизации

--- billing/src/domain/services/auth_service.py
+++ billing/src/domain/services/auth_service.py
@@ -1,14 +1,61 @@
 from abc import ABC, abstractmethod
 from uuid import UUID
 
+from grpc.aio import insecure_channel
+
+from infrastructure.auth_proto.auth_pb2 import ChangeRoleRequest, ResponseStatuses
+from infrastructure.auth_proto.auth_pb2_grpc import AuthNotifyStub
+
 
 class AuthService(ABC):
     """Сервис авторизации."""
 
     @abstractmethod
-    async def add_subscriber_status(self, user_id: UUID) -> None:
+    async def add_subscriber_status(self, user_id: UUID) -> bool:
         """Добавить статус подписчика пользователю.
 
         Args:
             user_id (UUID): Id пользователя.
         """
+
+
+class Auth(AuthService):
+    """Интерфейс взаимодействия с сервисом авторизации."""
+
+    subscriber = "subscriber"
+    unsubscribe = "unsubscribe"
+
+    def __init__(self, host: str):
+        self.host = host
+
+    async def add_subscriber_status(self, user_id: UUID) -> bool:
+        """Добавить статус подписчика пользователю.
+
+        Args:
+            user_id (UUID): Id пользователя.
+
+        Returns:
+            request result(bool): Успешность запроса.
+        """
+        async with insecure_channel(self.host) as channel:
+            stub = AuthNotifyStub(channel)
+            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.subscriber))
+            if response == ResponseStatuses.Value("OK"):
+                return True
+            return False
+
+    async def del_subscriber_status(self, user_id: UUID) -> bool:
+        """Удалить статус подписчика пользователю.
+
+        Args:
+            user_id (UUID): Id пользователя.
+
+        Returns:
+            request result(bool): Успешность запроса.
+        """
+        async with insecure_channel(self.host) as channel:
+            stub = AuthNotifyStub(channel)
+            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.unsubscribe))
+            if response == ResponseStatuses.Value("OK"):
+                return True
+            return False
