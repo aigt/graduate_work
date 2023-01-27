e54049e5431f1dc2e823698b458f9795b8573027
wood <amper.sil.tok@gmail.com>
2023-01-27 08:57:32 +0300

    Добавлены исключения в конфигурации линтеров

--- billing/src/domain/services/auth_service.py
+++ billing/src/domain/services/auth_service.py
@@ -2,11 +2,6 @@ from abc import ABC, abstractmethod
 
 from domain.aggregates_model.user_aggregate.user_id import UserId
 
-from grpc.aio import insecure_channel
-
-from infrastructure.auth_proto.auth_pb2 import ChangeRoleRequest, ResponseStatuses
-from infrastructure.auth_proto.auth_pb2_grpc import AuthNotifyStub
-
 
 class AuthService(ABC):
     """Сервис авторизации."""
@@ -18,45 +13,3 @@ class AuthService(ABC):
         Args:
             user_id (UserId): Id пользователя.
         """
-
-
-class Auth(AuthService):
-    """Интерфейс взаимодействия с сервисом авторизации."""
-
-    subscriber = "subscriber"
-    unsubscribe = "unsubscribe"
-
-    def __init__(self, host: str):
-        self.host = host
-
-    async def add_subscriber_status(self, user_id: UserId) -> bool:
-        """Добавить статус подписчика пользователю.
-
-        Args:
-            user_id (UserId): Id пользователя.
-
-        Returns:
-            request result(bool): Успешность запроса.
-        """
-        async with insecure_channel(self.host) as channel:
-            stub = AuthNotifyStub(channel)
-            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.subscriber))
-            if response == ResponseStatuses.Value("OK"):
-                return True
-            return False
-
-    async def del_subscriber_status(self, user_id: UserId) -> bool:
-        """Удалить статус подписчика пользователю.
-
-        Args:
-            user_id (UserId): Id пользователя.
-
-        Returns:
-            request result(bool): Успешность запроса.
-        """
-        async with insecure_channel(self.host) as channel:
-            stub = AuthNotifyStub(channel)
-            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.unsubscribe))
-            if response == ResponseStatuses.Value("OK"):
-                return True
-            return False
--- billing/src/infrastructure/auth_service/movie_auth_service.py
+++ billing/src/infrastructure/auth_service/movie_auth_service.py
@@ -1,13 +1,48 @@
+from grpc.aio import insecure_channel
+
 from domain.aggregates_model.user_aggregate.user_id import UserId
 from domain.services.auth_service import AuthService
+from infrastructure.auth_service.auth_pb2 import ChangeRoleRequest, ResponseStatuses
+from infrastructure.auth_service.auth_pb2_grpc import AuthNotifyStub
 
 
 class MovieAuthService(AuthService):
     """Сервис авторизации кинотеатра."""
 
-    async def add_subscriber_status(self, user_id: UserId) -> None:
+    subscriber = "subscriber"
+    unsubscribe = "unsubscribe"
+
+    def __init__(self, host: str):
+        self.host = host
+
+    async def add_subscriber_status(self, user_id: UserId) -> bool:
         """Добавить статус подписчика пользователю.
 
         Args:
             user_id (UserId): Id пользователя.
+
+        Returns:
+            request result(bool): Успешность запроса.
         """
+        async with insecure_channel(self.host) as channel:
+            stub = AuthNotifyStub(channel)
+            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.subscriber))
+            if response == ResponseStatuses.Value("OK"):
+                return True
+            return False
+
+    async def del_subscriber_status(self, user_id: UserId) -> bool:
+        """Удалить статус подписчика пользователю.
+
+        Args:
+            user_id (UserId): Id пользователя.
+
+        Returns:
+            request result(bool): Успешность запроса.
+        """
+        async with insecure_channel(self.host) as channel:
+            stub = AuthNotifyStub(channel)
+            response = stub.SetUserRole(ChangeRoleRequest(user_id=str(user_id), role=self.unsubscribe))
+            if response == ResponseStatuses.Value("OK"):
+                return True
+            return False
