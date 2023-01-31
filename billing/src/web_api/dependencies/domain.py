72494fa9e82119c15b56a4d015112f655a9d386c
aigt <ai-kr@yandex.ru>
2023-01-31 23:13:48 +0500

    Добавлена зависимость get_user

---
+++ billing/src/web_api/dependencies/domain.py
@@ -0,0 +1,18 @@
+from fastapi import Depends
+
+from domain.aggregates_model.user_aggregate.user import User
+from web_api.dependencies.common import get_jwt_service
+from web_api.services.jwt import JWTService
+
+
+def get_user(jwt_service: JWTService = Depends(get_jwt_service)) -> User:
+    """Фабрика пользователей.
+
+    Args:
+        jwt_service (JWTService): Depends(get_jwt_service).
+
+    Returns:
+        User: Сущность пользователя.
+    """
+    user_id = jwt_service.get_user_id()
+    return User(id=user_id)
