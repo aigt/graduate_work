4d567da07dd2bc3fd18f19b83f7b758d7021062c
wood <amper.sil.tok@gmail.com>
2023-01-31 12:45:14 +0300

    Добавлены валидаторы для дескрипторов

--- billing/src/domain/aggregates_model/external_refund_aggregate/external_refund_id.py
+++ billing/src/domain/aggregates_model/external_refund_aggregate/external_refund_id.py
@@ -2,11 +2,20 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from domain.seed_work.descriptors import ReadOnlyValidatebleDescriptor
+from domain.seed_work.descriptors import ReadOnlyValidatebleDescriptor, V
 
 
 class ExternalRefundIdField(ReadOnlyValidatebleDescriptor):
     """Дескриптор идентификатора возврата."""
+
+    def validate(self, value_to_validate: V) -> None:
+        """Метод валидации.
+
+        Args:
+            value_to_validate (V): Значение.
+        """
+        if not isinstance(value_to_validate, ExternalRefundId):
+            raise TypeError()
 
 
 @dataclass(frozen=True, slots=True)
