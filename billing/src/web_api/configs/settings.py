ba5a95857a5c85ea01573876d3158e23647399f9
aigt <ai-kr@yandex.ru>
2023-02-01 22:10:25 +0500

    Добавлены настройки хостов

--- billing/src/web_api/configs/settings.py
+++ billing/src/web_api/configs/settings.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     project_name: str = Field(default="billing")
     api_version: str = "1.0.0"
 
+    host = "localhost"
+
     auth_rsa_public_key = ""
 
     # Роуты
@@ -20,3 +22,6 @@ class Settings(BaseSettings):
 
     # Stripe
     endpoint_secret: str = ""
+
+    # Auth service
+    auth_service_host = "localhost"
