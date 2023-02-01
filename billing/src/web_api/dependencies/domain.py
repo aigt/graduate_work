2a2031f5ec52f2fa2ad3cf3aec74da6f1185c5d9
aigt <ai-kr@yandex.ru>
2023-02-01 21:27:40 +0500

    id заменён на ValueObject

--- billing/src/web_api/dependencies/domain.py
+++ billing/src/web_api/dependencies/domain.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 
 from domain.aggregates_model.user_aggregate.user import User
+from domain.aggregates_model.user_aggregate.user_id import UserId
 from web_api.dependencies.common import get_jwt_service
 from web_api.services.jwt import JWTService
 
@@ -14,5 +15,5 @@ def get_user(jwt_service: JWTService = Depends(get_jwt_service)) -> User:
     Returns:
         User: Сущность пользователя.
     """
-    user_id = jwt_service.get_user_id()
+    user_id = UserId(id=jwt_service.get_user_id())
     return User(id=user_id)
