f009753b6b1281026cac2fa2509c374bf6793050
wood <amper.sil.tok@gmail.com>
2023-02-03 09:20:35 +0300

    Добавлены настройки для переменных среды

--- scheduler/core/settings.py
+++ scheduler/core/settings.py
@@ -1,1 +1,20 @@
-"""Настройки."""
+from functools import lru_cache
+
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """Переменные окружения."""
+
+    postgres: str = Field("dbname = pyment_history user=app password=postgres host=localhost port=5432")
+    interval: int = Field(600)
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    """Фабрика настроек.
+
+    Returns:
+        Settings: Класс с настройками приложения.
+    """
+    return Settings()
