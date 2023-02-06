c662e2228e252b212b81fe864ab91073c8adddff
wood <amper.sil.tok@gmail.com>
2023-02-06 08:20:39 +0300

    актуализированы настройки, добавлен тест доступа к ендпоинту покупки подписки

--- tests/integration/billing_api/tests/test_buy_subscription.py
+++ tests/integration/billing_api/tests/test_buy_subscription.py
@@ -1,5 +1,10 @@
+from http import HTTPStatus
+
 from psycopg import Cursor
 from requests import Session  # type: ignore
+from settings import get_settings
+
+settings = get_settings()
 
 
 def test_buy(postgres_cur: Cursor, http_con: Session) -> None:
@@ -9,3 +14,17 @@ def test_buy(postgres_cur: Cursor, http_con: Session) -> None:
         postgres_cur(Cursor): Курсор постгреса
         http_con(Session): http сессия
     """
+
+
+def test_buy_unauthorized(http_con: Session) -> None:
+    """Тест доступа к ендпоинту для покупки подписки.
+
+    Args:
+        http_con(Session): http сессия
+    """
+    body = None
+    response = http_con.post(
+        f"{settings.url}/payments/pay_for_subscription",
+        json=body,
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
