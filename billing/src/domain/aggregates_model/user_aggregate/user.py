28ed3adb292469677dad8cd8f4656a9ddc79945a
aigt <ai-kr@yandex.ru>
2023-02-07 22:35:28 +0500

    Добавлен метод отмены платежа

--- billing/src/domain/aggregates_model/user_aggregate/user.py
+++ billing/src/domain/aggregates_model/user_aggregate/user.py
@@ -1,5 +1,16 @@
+from decimal import Decimal
+
 from domain.aggregates_model.external_payment_aggregate.external_payment import (
     ExternalPayment,
+)
+from domain.aggregates_model.external_payment_aggregate.external_payment_amount import (
+    ExternalPaymentAmount,
+)
+from domain.aggregates_model.external_refund_aggregate.external_refund_amount import (
+    ExternalRefundAmount,
+)
+from domain.aggregates_model.external_refund_aggregate.external_refund_payment_id import (
+    ExternalRefundPaymentId,
 )
 from domain.aggregates_model.payment_aggregate.payment_reposytory import (
     PaymentRepository,
@@ -20,22 +31,44 @@ class User:
 
     async def pay_for_subscription(
         self,
+        subscription_price: Decimal,
         payment_system: PaymentSystem,
         payment_repository: PaymentRepository,
     ) -> ExternalPayment:
         """Оплатить подписку.
 
         Args:
+            subscription_price (Decimal): Стоимость подписки.
             payment_system (PaymentSystem): Платёжная система.
             payment_repository (PaymentRepository): Репозторий платежей.
 
         Returns:
             ExternalPayment: Данные о созданном платеже.
         """
-        ext_payment: ExternalPayment = await payment_system.create_payment()
+        amount = ExternalPaymentAmount(amount=subscription_price)
+        ext_payment: ExternalPayment = await payment_system.create_payment(amount=amount)
         await payment_repository.create_payment(
             user_id=PaymentUserId(id=self.id.id),
             external_payment=ext_payment,
             system_id=PaymentSystemId(id=payment_system.system_id),
         )
         return ext_payment
+
+    async def refund_subscription(
+        self,
+        payment_system: PaymentSystem,
+        payment_repository: PaymentRepository,
+    ) -> None:
+        """Возвратить деньги за подписку.
+
+        Args:
+            payment_system (PaymentSystem): Платёжная система.
+            payment_repository (PaymentRepository): Репозторий платежей.
+        """
+        payment = await payment_repository.get_last_by_user_id(user_id=self.id.id)
+
+        amount = ExternalRefundAmount(payment.amount.amount)
+        external_id = ExternalRefundPaymentId(id=payment.external_id)
+        await payment_system.create_refund(amount=amount, payment_id=external_id)
+
+        await payment_repository.refund_payment(payment_id=payment.id)
